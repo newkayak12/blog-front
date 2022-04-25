d92e36d3d24e2655e5123c41ccdf5200c423f36f
newkayak12 <newkayak12@gmail.com>
2022-04-25 21:13:10 +0900

    login vuelidate

--- src/views/user/login/index.vue
+++ src/views/user/login/index.vue
@@ -4,9 +4,11 @@
     <form action="">
       <label>로그인아이디</label>
       <input type="text" placeholder="로그인아이디" v-model="id">
+      <div v-if="v$.id.$error" style="color: red; margin-bottom: 15px;">아이디를 입력하세요</div>
       <label>로그인비밀번호</label>
       <input :type="isShowPassword" placeholder="로그인비밀번호" v-model="password">
 <!--      <button @click.prevent="fnShowPassword">show</button>-->
+      <div v-if="v$.password.$error" style="color: red; margin-bottom: 15px;">비밀번호를 입력하세요</div>
       <div class="btns">
         <input type="button" value="로그인" @click="fnLogin" @keyup.enter="fnLogin">
         <button @click="fnJoin">회원가입</button>
@@ -22,9 +24,16 @@
 <script>
 import UserSvc from "@/service/UserSvc";
 import {useToast} from 'vue-toastification'
+import {required} from "@vuelidate/validators";
+import {useVuelidate} from "@vuelidate/core";
 
 export default {
   name: "index",
+  setup(){
+    return {
+      v$:useVuelidate()
+    }
+  },
   data() {
     return {
       isShowPassword:'password',
@@ -43,6 +52,11 @@ export default {
       this.$router.push({path:'/join'})
     },
     async fnLogin(){
+      this.v$.$touch();
+      if(this.v$.$invalid){
+        return false
+      }
+
       const response = await UserSvc.signIn({userId:this.id, userPassword:this.password});
       console.log(response)
       if(response.data.code === 200) {
@@ -62,7 +76,16 @@ export default {
     },
 
   },
-  components: {
+  validations() {
+    return {
+      id:{
+        required
+      },
+      password:{
+        required
+      }
+    }
+
   }
 }
 </script>
